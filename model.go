dc82f9e473bf5180c85afa789f757d39fb2f82eb
Hector Sanjuan <code@hector.link>
2021-01-11 15:42:36 +0100

    Fix model: update table name. Store Price as int

--- model.go
+++ model.go
@@ -12,9 +12,9 @@ const schema = "filquotes"
 // Quote stores FIL price information.
 type Quote struct {
 	//lint:ignore U1000 hit for go-pg
-	tableName struct{} `pg:"filquotes.fil_usd_quotes"`
+	tableName struct{} `pg:"filquotes.fil_quotes"`
 	Height    int64    `pg:",pk,use_zero,notnull"`
-	Price     float64  `pg:",use_zero,notnull"`
+	Price     int64    `pg:",use_zero,notnull"`
 	Exchange  string   `pg:",pk,use_zero,notnull`
 	Currency  string   `pg:",pk,use_zero,notnull`
 }
@@ -23,11 +23,20 @@ type Quote struct {
 func NewQuote(h int64, ex string, q quotetracker.Quote) Quote {
 	return Quote{
 		Height:   h,
-		Price:    q.Amount,
+		Price:    toMicroFIL(q.Amount),
 		Exchange: ex,
 		Currency: q.Pair.Buy.Symbol(),
 	}
 }
+
+// toMicroFIL converts FIL price to int for storage.
+func toMicroFIL(price float64) int64 {
+	return int64(price * 1000000) // microFIL
+}
+
+// func fromMicroFIL(price int64) float64 {
+// 	return float64(price / 1000000) // microFIL
+// }
 
 // Persist uses a transaction to insert a quote in the DB.
 func (q Quote) Persist(ctx context.Context, tx *pg.Tx) error {
