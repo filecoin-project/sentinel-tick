0f1919180b90369c4e8458968efdea1a7a5a86ee
Mike Greenberg <placer14@users.noreply.github.com>
2021-12-17 16:05:30 -0500

    chore: Remove allowance of 0 for Height field

--- model.go
+++ model.go
@@ -13,7 +13,7 @@ const schema = "currencies"
 type Quote struct {
 	//lint:ignore U1000 hit for go-pg
 	tableName     struct{} `pg:"currencies.quotes"`
-	Height        int64    `pg:",pk,notnull,use_zero"`
+	Height        int64    `pg:",pk,notnull"`
 	Price         int64    `pg:",notnull,use_zero"`
 	VolumeBase24h int64    `pg:"volume_base_24h,notnull,use_zero"`
 	Exchange      string   `pg:",pk,notnull"`
